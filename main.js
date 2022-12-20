959db337445560ae331348fa6e162a75311fb51b
Sathviek <71971661+Sathviek@users.noreply.github.com>
2022-12-20 19:03:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+let latitude, longitude, destination;
+
+$(document).ready(function () {
+    alert("Please allow the device to know your location!")
+    initGeolocation();
+})
+
+$(function () {
+    $("#navigate-button").click(function () {
+        window.location.href = `ar_wheather.html?source=${latitude};${longitude}&destination=${destination[0]};${destination[0]}`
+    })
+})
+
+function initGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success);
+    }
+    else {
+        alert("Sorry, your browser does not support geolocation services.");
+    }
+}
+
+function success(position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+
+    // Initializing Mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [longitude, latitude],
+        zoom: 16
+    });
+
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        })
+    );
+
+    map.addControl(
+      new MapboxGeocoder({
+            accessToken:mapboxgl.accessToken,
+            mapboxgl:mapboxgl
+      }).on('result',function(e) {
+        destination=e.result.center
+      })
+        
+       
+    );
+    'top-left'
+
+    map.on('click', function (e) {
+        console.log(e);
+        destination = e.lngLat;
+    });
+}
+
